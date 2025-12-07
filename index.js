123295e6f3b764f8d0358a5f6f2d75064243c0cb
Erin Jannusch <ejannusch1@outlook.com>
2025-12-06 16:27:52 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,48 @@
+// template_thil8q4
+// service_fgctz7f
+// o6ScdC0gQIrob-U-9
+
+let isModalOpen = false;
+let contrastToggle = false;
+
+function toggleContrast() {
+    contrastToggle = !contrastToggle;
+    if (contrastToggle) {
+        document.body.classList += " dark-theme"
+    }
+    else {
+        document.body.classList.remove("dark-theme")
+    }
+}
+
+function contact(event) {
+    event.preventDefault();
+    const loading = document.querySelector('.modal__overlay--loading');
+    const success = document.querySelector('.modal__overlay--success');
+    loading.classList += " modal__overlay--visible";
+
+    emailjs
+        .sendForm(
+            'service_fgctz7f',
+            'template_thil8q4',
+            event.target,
+            'o6ScdC0gQIrob-U-9'
+    ).then(() => {
+      loading.classList.remove("modal__overlay--visible");
+      success.classList += " modal__overlay--visible";
+    }).catch(() => {
+        loading.classList.remove("modal__overlay--visible");
+        alert(
+            "The email service is temporarily unavailable. Please contact me directly at ejannusch1@outlook.com"
+        );
+    })
+}
+
+function toggleModal() {
+    if (isModalOpen) {
+        isModalOpen = false;
+        return document.body.classList.remove("modal--open");
+    }
+    isModalOpen = true;
+    document.body.classList += " modal--open"
+}
